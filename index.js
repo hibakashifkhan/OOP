dcfee29ec139819d64a2ca5b57fa6261e5243ae7
unknown <hibakashifkhan1@gmail.com>
2024-06-22 21:36:04 +0500

    Adding an OOP

---
+++ index.js
@@ -0,0 +1,104 @@
+// import inquirer from "inquirer";
+// class Student {
+//     name : string
+//     constructor(n : string){
+//         this.name = n
+//     }
+// }
+// class Person{
+//     students : Student[]=[]
+//     addStudent(obj : Student){
+//         this.students.push(obj)
+//     }
+// }
+// const persons = new Person()
+// const programStart = async(person : Person)=>{
+//     do{
+//     console.log("Welcome!");
+//     const ans = await inquirer.prompt({
+//         name : "select",
+//         type : "list",
+//         message : "Who would you like to contact with?",
+//         choices : ["Staff","Student","Exit"],
+//     })
+//     if (ans.select == "Staff"){
+//         console.log("You have successfully approached the Staff room. Never hesitate to ask any questions.")
+//     }
+//     else if (ans.select == "Student"){
+//         const ans = await inquirer.prompt({
+//             name :"student",
+//             type : "input",
+//             message : "Enter the Student's Name you wish to contact with.",
+//         })
+//         const student = persons.students.find(val => val.name == ans.student)
+//     if(!student){
+//         const name = new Student(ans.student)
+//         persons.addStudent(name)
+//         console.log(`Hi! I'm ${name.name}. Its Good to Meet you!`)
+//         console.log("Student Successfully Added.");
+//         console.log("Current Student List")
+//         console.log(persons.students)
+//     }else {
+//         console.log(`Hi! I'm ${student.name}.Nice to Meet you again!`)
+//         console.log("Existing Student List :")
+//         console.log(persons.students);
+//     }
+// }else if (ans.select == "Exit"){
+//     console.log("Exiting...");
+//     process.exit;
+// }
+//     }while(true)
+// }
+// programStart(persons)
+import inquirer from "inquirer";
+class Student {
+    name;
+    constructor(n) {
+        this.name = n;
+    }
+}
+class Person {
+    students = [];
+    addStudent(obj) {
+        this.students.push(obj);
+    }
+}
+const person = new Person();
+const programStart = async (person) => {
+    while (true) {
+        console.log("Welcome!");
+        const ans = await inquirer.prompt({
+            name: "select",
+            type: "list",
+            message: "Who would you like to contact with?",
+            choices: ["Staff", "Student", "Exit"],
+        });
+        if (ans.select == "Staff") {
+            console.log("You have successfully approached the Staff room. Never hesitate to ask any questions.");
+        }
+        else if (ans.select == "Student") {
+            const ans = await inquirer.prompt({
+                name: "student",
+                type: "input",
+                message: "Enter the Student's Name you wish to contact with.",
+            });
+            const student = person.students.find(val => val.name === ans.student);
+            if (!student) {
+                const newStudent = new Student(ans.student);
+                person.addStudent(newStudent);
+                console.log(`Hi! I'm ${newStudent.name}. It's good to meet you!`);
+                console.log("Student successfully added.");
+            }
+            else {
+                console.log(`Hi! I'm ${student.name}. Nice to meet you again!`);
+            }
+            console.log("Current student list:");
+            person.students.forEach(s => console.log(s.name));
+        }
+        else if (ans.select == "Exit") {
+            console.log("Exiting...");
+            process.exit(0);
+        }
+    }
+};
+programStart(person);
